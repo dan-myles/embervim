16fc2230c7742e00a008b80baa9e7b25b3a099ec
Dan <79137382+danlikestocode@users.noreply.github.com>
2023-03-22 20:28:53 -0700

    fix: linkage:

--- nvim/after/plugin/comment.lua
+++
@@ -1,9 +0,0 @@
-require("Comment").setup({
-    mappings = {
-        basic = false,
-        extra = false,
-    }
-})
-
-vim.keymap.set('n', '<leader>/', '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>')
-vim.keymap.set('v', '<leader>/', '<ESC><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
