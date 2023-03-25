bafce667a5449152b66d4fd148108f6719149eb3
danlikestocode <79137382+danlikestocode@users.noreply.github.com>
2023-03-24 18:30:52 -0700

    updates

--- nvim/lua/default/remap.lua
+++ nvim/lua/default/remap.lua
@@ -39,4 +39,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+vim.keymap.set("n", "<leader>q", "<cmd>Alpha<CR>")
+
 
