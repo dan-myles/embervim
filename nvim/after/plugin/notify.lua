c05e90a3182444e59c84ba16c92ccfd7b7fb214e
danlikestocode <79137382+danlikestocode@users.noreply.github.com>
2023-04-08 14:51:33 -0700

    chore: migrating to lazy

--- nvim/after/plugin/notify.lua
+++
@@ -1,3 +0,0 @@
-require("notify").setup({
-    background_colour = "#1e222a",
-})
--- nvim/lua/embervim/set.lua
+++ nvim/lua/embervim/set.lua
@@ -39,3 +39,6 @@ vim.g.netrw_banner = 0
 vim.g.netrw_browse_split = 0
 vim.g.netrw_winsize = 25
 
+-- Fold Settings
+vim.o.foldenable = false
+
